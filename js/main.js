3fb78194eee833b190bdcb82dc046f5480e07204
Pablo Perez <paperezpablo@gmail.com>
2025-05-11 18:48:37 -0300

    endCards

--- js/main.js
+++ js/main.js
@@ -1,26 +1,14 @@
-// Desplegar menu hamburguersa
 const menuHamburguer = document.querySelector(".menu-hamburguer");
 const navLinks = document.querySelector(".navLinks");
 
-menuHamburguer.addEventListener("click", () => {
-  // cambiamos el icono
-  menuHamburguer.classList.toggle("fa-bars");
+// Activar y desactivar el menú cuando se hace clic en el icono de hamburguesa
+menuHamburguer.addEventListener("click", function() {
+  navLinks.classList.toggle("active"); // Alterna la clase 'active'
+
   menuHamburguer.classList.toggle("fa-xmark");
-  // Abrir o cerrar el menú
-  // const isActive = navLinks.classList.toggle("active");
-  // Le damos estilos cuando este activo
-  menuHamburguer.style.color = isActive ? "var(--second-color)" : "var(--first-color)";
+
 });
-// Agregar evento de clic a cada enlace del menú
-const menuLinks = document.querySelectorAll(".navLinks ul li a");
-menuLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    navLinks.classList.remove("active");
-    menuHamburguer.classList.remove("fa-xmark");
-    menuHamburguer.classList.add("fa-bars");
-    menuHamburguer.style.color = "var(--first-color)";
-  });
-});
+
 
 //--------- Swipper
 const swiper = new Swiper('.mySwiper', { 
@@ -40,17 +28,17 @@ const swiper = new Swiper('.mySwiper', {
 // ------------ Scroll del menu
 document.addEventListener("DOMContentLoaded", function () {
   const navbar = document.querySelector(".navbar");
-  let insNavbarFixed = false;
+  let isNavbarFixed  = false;
 
   window.addEventListener("scroll", function () {
     const scrollY = window.scrollY;
   
-    if (scrollY > 50 && !insNavbarFixed) {
+    if (scrollY > 50 && !isNavbarFixed ) {
       navbar.classList.add("fixed");
-      isNavbarFixed = true;
-    } else if (scrollY <= 50 && insNavbarFixed) {
+      isNavbarFixed  = true;
+    } else if (scrollY <= 50 && isNavbarFixed ) {
       navbar.classList.remove("fixed");
-      isNavbarFixed = false;
+      isNavbarFixed  = false;
     }
   });
 });
